798e918c443cd8ed6d4a5954944463f70f0273fa
bouzenaali <Alibouzena16@gmail.com>
2023-12-21 23:33:35 +0100

    feat: add pagination

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import httpx
 from selectolax.parser import HTMLParser
 
+def get_html(base_url, page):
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    resp = httpx.get(base_url + str(page), headers=headers, follow_redirects=True)
+    html = HTMLParser(resp.text)
+    return html
 
-url = "https://www.rei.com/c/camping-and-hiking/f/scd-deals"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-
-resp = httpx.get(url, headers=headers)
-html = HTMLParser(resp.text)
 
 def extract_text(html, sel):
     try:
         return html.css_first(sel).text()
     except AttributeError:
         return None
+    
 
-products = html.css("li.VcGDfKKy_dvNbxUqm29K")
+def parse_page(html):
+    products = html.css("li.VcGDfKKy_dvNbxUqm29K")
 
-for product in products:
-    item = {
-        "name": extract_text(product, ".Xpx0MUGhB7jSm5UvK2EY"),
-        "price":extract_text(product, "span[data-ui=sale-price]"),
-        "savings":extract_text(product, "div[data-ui=savings-percent-variant2]"),
-    }
-    print(item)
+    for product in products:
+        item = {
+            "name": extract_text(product, ".Xpx0MUGhB7jSm5UvK2EY"),
+            "price":extract_text(product, "span[data-ui=sale-price]"),
+            "savings":extract_text(product, "div[data-ui=savings-percent-variant2]"),
+        }
+        print(item)
+
+
+
+def main():
+    baseurl = "https://www.rei.com/c/camping-and-hiking/f/scd-deals?page="
+    for x in range(1, 10):
+        print(x)
+        html = get_html(baseurl, x)
+        parse_page(html)
+
+
+if __name__ == "__main__":
+    main()
