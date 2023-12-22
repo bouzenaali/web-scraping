5c8a16298953a8859f00c8d1834ca4a4fb5c9ab7
bouzenaali <Alibouzena16@gmail.com>
2023-12-22 14:38:42 +0100

    feat: store the data collected in a data class

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import httpx
 from selectolax.parser import HTMLParser
 import time
 from urllib.parse import urljoin
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class item:
+    name: str | None
+    item_num: str | None
+    price: str | None
+    rating: float | None
 
 
 def get_html(url, **Kwargs):
@@ -33,16 +42,26 @@ def extract_text(html, sel):
         return None
     
 
-def parse_search_page(html: HTMLParser):
+def parse_search_page(html):
     products = html.css("li.VcGDfKKy_dvNbxUqm29K")
 
     for product in products:
         yield urljoin("https://www.rei.com/", product.css_first("a").attrs["href"])
 
 
+def parse_item_page(html):
+    new_item = item(
+        name=extract_text(html, "h1#product-page-title" ),
+        item_num=extract_text(html, "span#product-item-number"),
+        price=extract_text(html, "span#buy-box-product-price"),
+        rating=extract_text(html, "span.rating__number_13"),
+    )
+    return new_item
+
 
 
 def main():
+    products = []
     baseurl = "https://www.rei.com/c/camping-and-hiking/f/scd-deals?page="
     for x in range(1, 2):
         print(f"Getting page: {x}")
@@ -53,8 +72,10 @@ def main():
         for url in product_urls:
             print(url)
             html = get_html(url)
-            print(html.css_first("title").text())
-            time.sleep(1)
+            products.append(parse_item_page(html))
+
+        for product in products:
+            print(asdict(product))
 
 if __name__ == "__main__":
     main()
